de238b0ac170c2c2bcaaedbf15346483085ecc87
ESE-Wahaj <wahaj.naveed16@gmail.com>
2024-05-11 12:15:52 +0500

    some app adjustments

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jaaw.app',
+  appName: 'jaaw',
+  webDir: 'build'
+};
+
+export default config;
